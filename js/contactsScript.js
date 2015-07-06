da652188326fc27463ee41ba0bc91107f845dc95
AnushaDahiya <malik.anusha@gmail.com>
2015-07-06 16:20:39 -0500

    Update contactsScript.js

--- js/contactsScript.js
+++ js/contactsScript.js
@@ -25,19 +25,30 @@ addNewRow = function(){
 }
 
 deleteSelectedRow = function(){
-	var table = document.getElementById("contactsTable").tBodies[0];
-	var rowCount = table.rows.length;
-
-	// var i=1 to start after header
-	for(var i=1; i<rowCount; i++) {
-		var row = table.rows[i];
-		// index of td contain checkbox is 9
-		var chkbox = row.cells[9].getElementsByTagName('input')[0];
-		if('checkbox' == chkbox.type && true == chkbox.checked) {
+	var table = document.getElementById("contactsTable");
+	var rowLength = table.rows.length;
+	for(var i=rowLength-1; i>0; i-=1){
+	  var row = table.rows[i];
+	  var cellLength = row.cells.length;
+	  var chkbox = row.cells[11].getElementsByTagName('input')[0];
+	  if('checkbox' == chkbox.type && true == chkbox.checked) {
 			table.deleteRow(i);
 		 }
 	}
 }
+selectAllRows = function(){
+	var table = document.getElementById("contactsTable");
+	var rowLength = table.rows.length;
+	for(var i=rowLength-1; i>0; i-=1){
+	  var row = table.rows[i];
+	  var cellLength = row.cells.length;
+	  var chkbox = row.cells[11].getElementsByTagName('input')[0];
+	  if('checkbox' == chkbox.type) {
+			chkbox.checked = true;
+		 }
+	}
+}
+
 
 uploadContacts = function(){
 	//upload the contacts from here
